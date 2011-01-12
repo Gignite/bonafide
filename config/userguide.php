591e8269e7b44999b20df913fbfc34fd4acf1cc3
Woody Gilk <woody.gilk@kohanaframework.org>
2011-01-11 20:01:22 -0600

    Added userguide pages

---
+++ config/userguide.php
@@ -0,0 +1,19 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'modules' => array(
+		'bonafide' => array(
+			// Whether this modules userguide pages should be shown
+			'enabled' => TRUE,
+
+			// The name that should show up on the userguide index page
+			'name' => 'Bonafide',
+
+			// A short description of this module, shown on the index page
+			'description' => 'Flexible authentication.',
+
+			// Copyright message, shown in the footer for this module
+			'copyright' => '&copy; 2011 Woody Gilk',
+		),
+	),
+);
