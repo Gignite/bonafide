ede8668672c8074eb5566dbc3dc000d51803adc2
Woody Gilk <woody.gilk@gmail.com>
2011-01-12 11:04:54 -0600

    Do not include any mechanism configuration by default, configuration must be done by the end user!

--- config/bonafide.php
+++ config/bonafide.php
@@ -8,17 +8,9 @@ return array(
 		// Multiple mechanisms can be added for versioned passwords, etc
 		'mechanisms' => array(
 
-			// Bonafide ships with a HMAC hashing mechanism
-			array('hash', array(
-				// Hash prefix, must be unique for every mechanism!
-				// 'prefix' => NULL,
+			// Put your mechanisms here! The format is:
+			// string $id => array(string $mechanism, array $config)
 
-				// Hash type to use when calling hash_hmac()
-				// 'type' => 'sha256',
-
-				// Shared secret HMAC key
-				// 'key' => 'put your shared secret key here!',
-			)),
 		),
 	),
 );
