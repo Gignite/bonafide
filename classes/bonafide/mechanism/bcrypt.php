c1769e63456da454de782c1f0fb7b6bb132375f0
Woody Gilk <woody.gilk@kohanaframework.org>
2011-01-21 09:32:19 -0600

    Follow up to 3d46bbc6a1925e46cc05c84cddde8d8be946a42b, clean up code and fix inheritance errors

--- classes/bonafide/mechanism/bcrypt.php
+++ classes/bonafide/mechanism/bcrypt.php
@@ -1,54 +1,68 @@
 <?php defined('SYSPATH') or die('No direct script access.');
 /**
+ * Bona Fide is a flexible authentication system for the Kohana Framework.
+ *
  * This mechanism provides support for bcrypt password hashing.
- *
- * bcrypt is highly recommended by many to safely store passwords.
- * see: http://codahale.com/how-to-safely-store-a-password/
  * 
- * in short: bcrypt is slow, which makes brute force attacks very
- * time consuming. Moreover, bcrypt introduces a work
- * factor (here $cost). This allows the mechanism to keep up
- * with Moore's law. As computers get faster, you can increase
- * the work factor and the hash will get slower.
+ * bcrypt is highly recommended by many to safely store passwords. For more
+ * information, see http://codahale.com/how-to-safely-store-a-password/
  *
- * Please note that you CAN increase the work factor ($cost)
- * at any time, even in production. Existing hashes will remain
- * valid because the work factor is actually stored in the hash
- * itself!
+ * @package    Bona Fide
+ * @category   Mechanisms
+ * @author     Wouter <wouter.w@gmx.net>
+ * @author     Woody Gilk <woody.gilk@kohanaframework.org>
+ * @copyright  (c) 2011 Wouter
+ * @copyright  (c) 2011 Woody Gilk
+ * @license    MIT
  */
 class Bonafide_Mechanism_Bcrypt extends Bonafide_Mechanism {
 
 	/**
-	 * @param  int  A number between 4 and 31; the 'base-2 logarithm of the iteration count' --> 2^$cost
-	 *              higher = more iterations = slower encryption = more secure hash.
+	 * @param  integer  number between 4 and 31, base-2 logarithm of the iteration count
 	 */
 	public $cost = 12;
 
 	public function __construct(array $config = NULL)
 	{
-		if ( ! CRYPT_BLOWFISH)
+		if ( ! defined('CRYPT_BLOWFISH'))
 		{
-			throw new Bonafide_Exception('No Blowfish Encryption support on this system');
+			throw new Bonafide_Exception('This server does not support bcrypt hashing');
 		}
 
-		parent::_construct($config);
+		parent::__construct($config);
 	}
 
-	public function hash($password)
+	public function hash($password, $salt = NULL, $iterations = NULL)
 	{
-		$salt = Text::random('alnum', 22);
+		if ( ! $salt)
+		{
+			// Generate a random 22 character salt
+			$salt = Text::random('alnum', 22);
 
-		return $this->_hash($password, $salt, $this->cost);
+			// Apply 0 padding to the cost
+			$cost = sprintf('%02d', $this->cost);
+
+			// Create a salt suitable for bcrypt
+			$salt = '$2a$'.$cost.'$'.$salt.'$';
+		}
+
+		return $this->prefix.$this->_hash($password, $salt);
 	}
 
-	protected function _hash($input, $salt, $cost)
+	protected function _hash($input, $salt = NULL)
 	{
-		return crypt($input, '$2a$' . ($cost < 10 ? '0' : '') . (int) $cost . '$' . $salt . '$');
+		return crypt($input, $salt);
 	}
 
-	public function check($password, $hash)
+	public function check($password, $hash, $salt = NULL, $iterations = NULL)
 	{
-		return $hash === $this->_hash($password, substr($hash,7,22), substr($hash, 4, 2));
+		// Prefix may be adding length to the hash
+		$offset = strlen($this->prefix);
+
+		// $2a$ (4) $ (1) 00 (2) $ (1) <salt> (22)
+		$salt = substr($hash, $offset, 4 + 1 + 2 + 1 + 22 + $offset);
+
+		return $hash === $this->hash($password, $salt.'$');
 	}
 
 } // End Bonafide_Mechansim_Bcrypt
