06f6eb11f09f0a2105f3b8f244c9662dd29ab350
Woody Gilk <woody.gilk@kohanaframework.org>
2011-11-27 13:13:48 -0600

    Added demo config file

---
+++ config/demo.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+	'bonafide' => array(
+		'name'  => 'Bonafide',
+		'title' => 'Secure and Upgradable Hashing',
+		'url'   => 'https://github.com/shadowhand/bonafide',
+	),
+);
